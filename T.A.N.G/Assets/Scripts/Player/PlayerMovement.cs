cb763e8ad60ee07d96b2c43011567a4c1096ba71
Skylar Schultz <skylar.schultz21@gmail.com>
2026-07-07 21:54:20 -0500

    Chapter 2: Player movement and jumping

---
+++ T.A.N.G/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    [SerializeField]
+    private float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private float horizontalInput;
+    private bool isGrounded = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
